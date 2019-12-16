0ad33425a6426a4e597a5e3762242d3740e65f6e
Ahmed Al Salih <a.alsalih2@gmail.com>
2019-12-15 22:20:17 -0500

    compleate the project then switch the implementation to support the web service application as well

--- src/main/java/com/atro/samlokta/controller/MainController.java
+++ src/main/java/com/atro/samlokta/controller/MainController.java
@@ -1,15 +1,15 @@
 package com.atro.samlokta.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class MainController {
 	
 	
-	@GetMapping("/")
+	@RequestMapping("/")
 	public String index() {
-		return "index";
+		return "Hello SAML!";
 	}
 
 }
