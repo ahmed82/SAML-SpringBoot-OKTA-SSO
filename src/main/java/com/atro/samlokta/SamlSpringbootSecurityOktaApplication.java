51aef02ed83526f7aa0506702faead352ac74062
Ahmed Al Salih <a.alsalih2@gmail.com>
2019-12-15 21:23:07 -0500

    init the project with required dependencies

---
+++ src/main/java/com/atro/samlokta/SamlSpringbootSecurityOktaApplication.java
@@ -0,0 +1,13 @@
+package com.atro.samlokta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamlSpringbootSecurityOktaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamlSpringbootSecurityOktaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atro/samlokta/SamlSpringbootSecurityOktaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atro.samlokta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamlSpringbootSecurityOktaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
